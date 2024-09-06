859c6308527de0a8313b936b1121ee219841b59d
Matheus Lui <matheuszago134@gmail.com>
2024-09-06 15:28:11 -0300

    Creating Spring project for Atividade4

---
+++ sprint-5-semana-05/atividade4/src/main/java/com/matheus/library/Atividade4Application.java
@@ -0,0 +1,13 @@
+package com.matheus.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividade4Application.class, args);
+	}
+
+}
